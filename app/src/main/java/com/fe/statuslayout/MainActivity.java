e4111d5e8227e95ba2840d135ac82abe31adf028
陈鹏飞 <pengfei.chen@weimob.com>
2017-03-14 18:11:30 +0800

    update bug

--- app/src/main/java/com/fe/statuslayout/MainActivity.java
+++ app/src/main/java/com/fe/statuslayout/MainActivity.java
@@ -1,20 +1,13 @@
 package com.fe.statuslayout;
 
-import android.os.Bundle;
-
-
+/**
+ * Created by chenpengfei on 2017/3/14.
+ */
 public class MainActivity extends BaseActivity {
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     protected int getContentView() {
         return R.layout.activity_content;
     }
-
 
 }
